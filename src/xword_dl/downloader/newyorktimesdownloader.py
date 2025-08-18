0d5f014c0dfbb0d1295152da09241f5a2fea81d4
Rohan Narayan <ronarayan@linkedin.com>
2025-08-17 18:27:40 -0700

    re-add nytm

--- src/xword_dl/downloader/newyorktimesdownloader.py
+++ src/xword_dl/downloader/newyorktimesdownloader.py
@@ -228,30 +228,3 @@ class NewYorkTimesVarietyDownloader(NewYorkTimesDownloader):
             raise XWordDLException(
                 "Encountered error while parsing data. Maybe the selected puzzle is not a crossword?"
             )
-
-
-class NewYorkTimesMiniDownloader(NewYorkTimesDownloader):
-    command = "nytm"
-    outlet = "New York Times Mini"
-    outlet_prefix = "NY Times Mini"
-
-    def __init__(self, **kwargs):
-        super().__init__(inherit_settings="nyt", **kwargs)
-
-        self.url_from_date = (
-            "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini/{}.json"
-        )
-
-    @classmethod
-    def matches_url(cls, url_components):
-        return "nytimes.com" in url_components.netloc and "mini" in url_components.path
-
-    def find_latest(self):
-        oracle = "https://www.nytimes.com/svc/crosswords/v2/oracle/mini.json"
-
-        res = requests.get(oracle)
-        puzzle_date = res.json()["results"]["current"]["print_date"]
-
-        url = self.url_from_date.format(puzzle_date)
-
-        return url
