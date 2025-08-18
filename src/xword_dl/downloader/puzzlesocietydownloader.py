aec77ede94102d639f3a7e8043545f1208e41f04
Rohan Narayan <ronarayan@linkedin.com>
2025-08-17 18:21:04 -0700

    remove more

--- src/xword_dl/downloader/puzzlesocietydownloader.py
+++ src/xword_dl/downloader/puzzlesocietydownloader.py
@@ -6,7 +6,8 @@ import requests
 
 from bs4 import BeautifulSoup, Tag
 
-from xword_dl.util.utils import XWordDLException
+#from xword_dl.util.utils import XWordDLException
+from util import XWordDLException
 
 from .compilerdownloader import CrosswordCompilerDownloader
 
